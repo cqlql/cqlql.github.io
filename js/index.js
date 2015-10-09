af8faebdceb4714a6bbdc33e782c5fd8a69741f6
cqlql <cql.ql@qq.com>
2015-10-09 18:14:49 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ ePager.innerHTML = c.pager.getHtml({
     prevTxt: '&lt;',//可选
     nextTxt: '&gt;'//可选
 
-    ,baseUrl: '/',//可选。默认为false。表示值为javascript:; 这种情况，pageUrl也可不选，即使选了也无效。
+    ,baseUrl: '',//可选。默认为false。表示值为javascript:; 这种情况，pageUrl也可不选，即使选了也无效。
     pageUrl: '/page',//可选。默认 '?page='。如果baseUrl为false，pageUrl选了也无效
 });
 
