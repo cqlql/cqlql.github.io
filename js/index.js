3437c8dbbcba3d440f0d5b3ca3efff641ee311c8
cqlql <cql.ql@qq.com>
2015-10-09 17:33:20 +0800

    翻页修改

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var ePager = document.getElementById('pager')
 , pageData = ePager.getAttribute('data-page').split(',');
 
 
-ePager.innerHTML = getHtml({
+ePager.innerHTML = c.pager.getHtml({
     pageData: [pageData[0], pageData[1], pageData[2]],//当前页，数据总条数，每页显示数
     prevCssName: 'prev',//可选
     nextCssName: 'next',//可选
