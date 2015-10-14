575918866fc6370f9fd707155e5fd6fde48cefd6
cqlql <cql.ql@qq.com>
2015-10-14 18:38:39 +0800

    修改demo

--- demo/20151013_滚动功能/main.js
+++ demo/20151013_滚动功能/main.js
@@ -1,19 +1,30 @@
 ﻿"use strict";
-
+//功能拆分
 
 var eBox = document.getElementById('demo1'),
-    eCont = eBox.children[0];
+    eCont = eBox.children[0],
+    type='top',
+    currContXY = {
+        left: 0,
+        top:0
+    },
+    tempContXY={};
 
-moveCont(100);
+//moveCont(100);
+
 
 c.drag(eBox, function (xy) {
-    console.log(xy);
+    currContXY[type] = tempContXY[type]+xy[type];
+
+    moveCont(currContXY[type]);
+
 }, function () {
+    tempContXY[type] = currContXY[type];
 
 }, function () {
 
 });
 
 function moveCont(v) {
-    eCont.style.top = v+'px';
+    eCont.style[type] = v + 'px';
 }
