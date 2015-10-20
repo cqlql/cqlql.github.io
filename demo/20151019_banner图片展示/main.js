1c40b4dfc8a50e8dceb569fbe44fb494a620771a
cqlql <cql.ql@qq.com>
2015-10-20 18:20:55 +0800

    增加 deom banner

--- demo/20151019_banner图片展示/main.js
+++ demo/20151019_banner图片展示/main.js
@@ -1,2 +1,31 @@
 ﻿"use strict";
 
+//为什么复杂，因为没有独立
+
+
+function bannerFade() {
+    var eBox = document.getElementById('fadeDemo')
+        ,eListBox=eBox.children[0]
+        , eItems = eListBox.children
+
+        , initShow = params.initShow || function () {
+
+        }
+
+        , currindex;
+
+
+
+    initShow();
+
+    this.toChange = toChange;
+    this.initShow = initShow;
+
+    function toChange(i) {
+
+    }
+
+
+
+}
+bannerFade({});
