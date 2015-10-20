009951df020c70144774e024c5345fd0f8588aeb
cqlql <cql.ql@qq.com>
2015-10-20 20:31:14 +0800

    修改demo

--- demo/20151019_banner图片展示/main.js
+++ demo/20151019_banner图片展示/main.js
@@ -3,29 +3,34 @@
 //为什么复杂，因为没有独立
 
 
-function bannerFade() {
+function bannerFade(params) {
     var eBox = document.getElementById('fadeDemo')
         ,eListBox=eBox.children[0]
         , eItems = eListBox.children
 
-        , initShow = params.initShow || function () {
-
-        }
+        // 接受三种值：不给(0索引)、false(不进行初始显示)、索引值
+        , isInitShow =params.isInitShow === undefined ? 0 : params.isInitShow 
 
         , currindex;
-
-
-
-    initShow();
+    
+    
+    if (isInitShow !== false) initShow(isInitShow);
 
     this.toChange = toChange;
     this.initShow = initShow;
 
-    function toChange(i) {
+    function toChange(index) {
+
+    }
+
+    function initShow(index) {
+
 
     }
 
 
 
 }
+
+
 bannerFade({});
