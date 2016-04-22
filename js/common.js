1eb1d53e5bb5a9b9480e8760e8c2674b77adff75
cqlql <cql.ql@qq.com>
2016-04-22 17:19:30 +0800

    新增功能

--- js/common.js
+++ js/common.js
@@ -1360,6 +1360,138 @@ window.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAni
     };
     //#endregion
 
+    //#region JSON处理
+    c.JSON = {
+        // 对象转换为 json数据。解决低端浏览器不支持JSON.stringify
+        stringify: function (obj) {
+            var data = '';
+
+            if (getType(obj) === 'object') {
+                fn(obj);
+                data = '{' + data.substr(0, data.length - 1) + '}';
+            }
+            else {
+                arrayFn(obj);
+                data = '[' + data.substr(0, data.length - 1) + ']';
+            }
+
+            return data;
+
+            function fn(obj) {
+                var val;
+                var type;
+                for (var key in obj) {
+
+                    val = obj[key];
+                    type = getType(val);
+
+                    if (type === 'object') {
+                        data += '"' + key + '":{';
+                        fn(val);
+                        data = data.substr(0, data.length - 1);
+                        data += '},';
+
+                    }
+                    else if (type === 'array') {
+                        data += '"' + key + '":[';
+                        arrayFn(val);
+                        data = data.substr(0, data.length - 1);
+                        data += '],';
+                    }
+                    else {
+                        data += '"' + key + '":"' + val + '",';
+                    }
+                }
+            }
+
+            function arrayFn(obj) {
+                var val;
+                var type;
+                for (var key in obj) {
+
+                    val = obj[key];
+                    type = getType(val);
+
+                    if (type === 'object') {
+                        data += '{';
+                        fn(val);
+                        data = data.substr(0, data.length - 1);
+                        data += '},';
+                    }
+                    else if (type === 'array') {
+                        data += '[';
+                        arrayFn(val);
+                        data = data.substr(0, data.length - 1);
+                        data += '],';
+                    }
+                    else {
+                        data += '"' + val + '",';
+                    }
+                }
+
+            }
+
+            function getType(v) {
+
+                var typeStr = typeof v,
+                    fullTypeStr;
+
+                if (typeStr === 'object') {
+                    fullTypeStr = ({}).toString.call(v);
+                    return /\[object ([^\]]+)\]/.exec(fullTypeStr)[1].toLowerCase();
+                }
+                else {
+                    return typeStr;
+                }
+            }
+
+        }
+        , parse: function () {
+
+        }
+        // 对象转表单数据
+        , formData: function (obj) {
+            var data = '';
+
+            fn(obj);
+
+            return data.substr(1);
+
+            function fn(obj, name) {
+
+                for (var key in obj) {
+
+                    var val = obj[key];
+                    var type = getType(val);
+                    var newName = name ? name + '[' + key + ']' : key;
+                    console.log(type);
+                    if (type === 'array' || type === 'object') {
+                        fn(val, newName);
+                    }
+                    else {
+                        data += '&' + newName + '=' + val;
+                    }
+                }
+            }
+
+            function getType(v) {
+
+                var typeStr = typeof v,
+                    fullTypeStr;
+
+                if (typeStr === 'object') {
+                    fullTypeStr = ({}).toString.call(v);
+                    return /\[object ([^\]]+)\]/.exec(fullTypeStr)[1].toLowerCase();
+                }
+                else {
+                    return typeStr;
+                }
+            }
+        }
+
+    };
+    //#endregion
+
     //#region 仿jq
     window.$ = window.jsDo = (function () {
         var deletedIds = [];
