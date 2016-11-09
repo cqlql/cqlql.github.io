776286074d0556c275056871dc1a6cdabf4be772
cqlql <cql.ql@qq.com>
2016-11-09 14:58:21 +0800

    删除demo 0

--- demo/0/start.cmd
+++
@@ -1,4 +0,0 @@
-::cd /d E:\Dropbox\__important\_implement\gulp
-::gulp TeachingEasy
-explorer http://localhost:8082/demo/0/
-::start cmd /k "%~d0&&sass --watch sass/common.scss:css/common.css --style expanded"
