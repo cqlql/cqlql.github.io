1326e00e79e8ddb4079296d9a2bd1d39058be4a8
cqlql <cql.ql@qq.com>
2017-08-16 18:16:08 +0800

    效果重构：分块饼图统计-native使用版本-demo版本

---
+++ demo/20170816_分块饼图统计/README.md
@@ -0,0 +1,2 @@
+
+[演示](http://cqlql.github.io/demo/20170816_%E5%88%86%E5%9D%97%E9%A5%BC%E5%9B%BE%E7%BB%9F%E8%AE%A1/dist/index.html)
---
+++ demo/20170816_分块饼图统计/src/test-data.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+transmitData([
+  {
+    "dimension_id": "12",
+    "dimension_name": "教学目标",
+    "score": "60",
+    "weight":20
+  },
+  {
+    "dimension_id": "36",
+    "dimension_name": "教学行为",
+    "score": "70",
+    "weight":30
+  },
+  {
+    "dimension_id": "37",
+    "dimension_name": "学习方式",
+    "score": "50",
+    "weight":30
+  },
+  {
+    "dimension_id": "38",
+    "dimension_name": "教学效果",
+    "score": "90",
+    "weight":20
+  }
+])
