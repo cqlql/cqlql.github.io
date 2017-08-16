ee9c4dd8d7047b5b276ad9b549c93f1a5c82340f
cqlql <cql.ql@qq.com>
2017-08-16 18:15:13 +0800

    效果重构：分块饼图统计-native使用版本-demo版本

--- demo/20170816_分块饼图统计/src/index.js
+++ demo/20170816_分块饼图统计/src/index.js
@@ -2,78 +2,94 @@ import './index.css'
 import getxyByRad from './getxy-by-rad'
 import pie from './pie'
 
-// 扇形占比
-// let data = [.1,.2,.3,.2,.1,.1]
-// let eachData=[.5,.3,.2,.6,.6,.7]
-// let labels=['课堂任务','课堂文化','课堂内容','课堂管理','策略','xx']
-let data = [.1,.2,.3,.2,.2]
-let eachData=[.5,.3,.7,.6,.5]
-let labels=['课堂任务','课堂文化','课堂内容','课堂管理','策略']
-let tscore = 86
-
+function chartPie({data,eachData,labels,tscore}) {
 
 // 圆心
-let ox = 100
-let oy = 100
+  let ox = 100
+  let oy = 100
 
 // 半径
-let r = 50
+  let r = 50
 
 // 圆心发散偏移值
-let oft = 1
+  let oft = 1
 
 // 底色值
-let bgcs = ['#fff1e7','#fff5e1','#ffefef','#f1f5ff','#edf7ff']
-let eachBgcs = ['#ffbb87','#ffc966','#ffa9ab','#bbc7ff','#a3d1ff']
-let lcs = ['#f96902','#ffad14','#ff4b4b','#5980ff','#5bacfe']
+  let bgcs = ['#fff1e7', '#fff5e1', '#ffefef', '#f1f5ff', '#edf7ff']
+  let eachBgcs = ['#ffbb87', '#ffc966', '#ffa9ab', '#bbc7ff', '#a3d1ff']
+  let lcs = ['#f96902', '#ffad14', '#ff4b4b', '#5980ff', '#5bacfe']
 
 // 起始弧度
-let sRad = Math.PI / 180 * -90
-let paths = ''
-let texts=''
-data.forEach(function (p,i) {
-  let rad = p * Math.PI * 2
+  let sRad = Math.PI / 180 * -90
+  let paths = ''
+  let texts = ''
+  data.forEach(function (p, i) {
+    let rad = p * Math.PI * 2
 
-  paths += `<path d="${pie({
-    ox,oy,r,sRad,rad,oft
-  })}"
-fill="${bgcs[i%5]}"
+    paths += `<path d="${pie({
+      ox, oy, r, sRad, rad, oft
+    })}"
+fill="${bgcs[i % 5]}"
 ></path>`
 
-  let r2 = r * eachData[i]
-  paths += `<path d="${pie({
-    ox,oy,r:r2,sRad,rad,oft
-  })}"
-fill="${eachBgcs[i%5]}"
+    let r2 = r * eachData[i]
+    paths += `<path d="${pie({
+      ox, oy, r: r2, sRad, rad, oft
+    })}"
+fill="${eachBgcs[i % 5]}"
 ></path>`
 
-  // 画线，并记录文本坐标
-  let ra = sRad+rad/2
-  // 末端折线偏移
-  let exOft = 6
-  if(180 / Math.PI*ra + 90>180){
+    // 画线，并记录文本坐标
+    let ra = sRad + rad / 2
+    // 末端折线偏移
+    let exOft = 6
+    if (180 / Math.PI * ra + 90 > 180) {
       exOft = -exOft
-  }
-  let {x:sx,y:sy} = getxyByRad(ra,r2/1.2,ox,oy)
-  let {x:ex,y:ey} = getxyByRad(ra,r+6,ox,oy)
-  let tx = ex+exOft,ty = ey
-  paths += `<polyline points="${sx},${sy} ${ex},${ey} ${tx},${ty}"
+    }
+    let {x: sx, y: sy} = getxyByRad(ra, r2 / 1.2, ox, oy)
+    let {x: ex, y: ey} = getxyByRad(ra, r + 6, ox, oy)
+    let tx = ex + exOft, ty = ey
+    paths += `<polyline points="${sx},${sy} ${ex},${ey} ${tx},${ty}"
 fill="none"
-stroke="${lcs[i%5]}"
+stroke="${lcs[i % 5]}"
 stroke-width="0.5"
 ></polyline>`
-  // div文本
-  texts+=`<div class="lb" style="left:${(tx)/200*100}%;top:${(ty)/200*100}%;"><span style="${exOft>0?'':'float:right;'}color:${lcs[i%5]}">${labels[i]}</span></div>`
-  // svg 文本
-  // texts+=`<text x="${tx}" y="${ty}" font-size="10" text-anchor="${exOft>0?'start':'end'}">${labels[i]}</text>`
+    // div文本
+    texts += `<div class="lb" style="left:${(tx) / 200 * 100}%;top:${(ty) / 200 * 100}%;"><span style="${exOft > 0 ? '' : 'float:right;'}color:${lcs[i % 5]}">${labels[i]}</span></div>`
+    // svg 文本
+    // texts+=`<text x="${tx}" y="${ty}" font-size="10" text-anchor="${exOft>0?'start':'end'}">${labels[i]}</text>`
 
-  sRad += rad
-})
+    sRad += rad
+  })
 
 // 总分
-texts+=`<div class="tscore">${tscore}分</div>`
+  texts += `<div class="tscore">${tscore}分</div>`
 
 // document.querySelector('.part-pie').innerHTML=`<svg viewbox="0,0,200,200">${paths}${texts}</svg>`
-document.querySelector('.part-pie').innerHTML=`<svg viewbox="0,0,200,200">${paths}</svg>${texts}`
+  document.querySelector('.part-pie').innerHTML = `<svg viewbox="0,0,200,200">${paths}</svg>${texts}`
+}
 
+window.transmitData = function (baseData) {
 
+  // 扇形占比
+  let data = []
+  let eachData = []
+  let labels = []
+  let tscore = 0
+
+  baseData.forEach(function (d) {
+    let {weight,score,dimension_name} = d
+    let w = weight/100
+    data.push(w)
+    eachData.push(score/100)
+    labels.push(dimension_name+':'+score+'分')
+    tscore+=score * w
+  })
+
+  chartPie({
+    data,
+    eachData,
+    labels,
+    tscore
+  })
+}
--- demo/20170816_分块饼图统计/webpack.config.js
+++ demo/20170816_分块饼图统计/webpack.config.js
@@ -6,96 +6,94 @@ let ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = function (env, options) {
 
-    let params = env ? env.split(',') : []
-    let dev = params[0] !== 'p'
+  let params = env ? env.split(',') : []
+  let dev = params[0] !== 'p'
 
-    let outputPath = path.resolve(__dirname, "dist")
+  let outputPath = path.resolve(__dirname, dev?"dist":'assets')
 
-    return {
-        entry: {
-            index: ["./src/index.js"]
-        },
+  return {
+    entry: {
+      index: ["./src/index.js"].concat(dev?"./src/test-data.js":[])
+    },
 
-        output: {
-            path: outputPath,
-            filename: "js/[name].js"
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                filename: './index.html',
-                template: './src/index.html',
-                chunks: ['index']
-            }),
-            new ExtractTextPlugin('css/[name].css'),
+    output: {
+      path: outputPath,
+      filename: "js/[name].js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: './index.html',
+        template: './src/index.html',
+        chunks: ['index']
+      }),
+      new ExtractTextPlugin('css/[name].css'),
+      // new webpack.NamedModulesPlugin(),
+      // new webpack.optimize.CommonsChunkPlugin({
+      //     name: ['common', 'manifest'],
+      // }),
 
-            // new webpack.optimize.CommonsChunkPlugin({
-            //     name: ['common', 'manifest'],
-            // }),
+      // new CleanWebpackPlugin(['dist']),// 清理dist
+      // new webpack.HotModuleReplacementPlugin(), // 启用 HMR
 
-            // new CleanWebpackPlugin(['dist']),// 清理dist
-            // new webpack.HotModuleReplacementPlugin(), // 启用 HMR
+    ],
 
-        ],
-
-        module: {
-            //加载器配置
-            rules: [
-                {
-                    test: /\.js$/,
-                    // exclude: /node_modules/,
-                    include: [path.resolve(__dirname, "src")],
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": ["env"],
-                        "plugins": ["transform-runtime","syntax-dynamic-import"],
-                    }
-                }, {
-                    test: /\.css$/,
-                    use: ExtractTextPlugin.extract({
-                        // fallback: 'style-loader',
-                        use: [{
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 1,
-                                sourceMap: true,
-                                url: false
-                            }
-                        },{
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: [
-                                    require('autoprefixer')({
-                                        remove: false
-                                    }),
-                                ]
-                            }
-                        }]
-                    }),
-                }
-            ]
-        },
-        resolve: {
-
-            // 寻找模块的目录
-            modules: [
-                path.resolve(__dirname, "../../node_modules")
-                // "node_modules"
-            ],
-
-            extensions: [".js"],
-
-            // 别名
-            alias: {
-
-            }
-        },
-
-        devServer: {
-            contentBase: outputPath,
-            compress: true,
-            host:'192.168.1.222',
-            port: 3001,
-            hot: false
+    module: {
+      //加载器配置
+      rules: [
+        {
+          test: /\.js$/,
+          // exclude: /node_modules/,
+          include: [path.resolve(__dirname, "src")],
+          loader: 'babel-loader',
+          options: {
+            "presets": ["env"],
+            "plugins": ["transform-runtime", "syntax-dynamic-import"],
+          }
+        }, {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            // fallback: 'style-loader',
+            use: [{
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                sourceMap: true,
+                url: false
+              }
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('autoprefixer')({
+                    remove: false
+                  }),
+                ]
+              }
+            }]
+          }),
         }
+      ]
+    },
+    resolve: {
+
+      // 寻找模块的目录
+      modules: [
+        path.resolve(__dirname, "../../node_modules")
+        // "node_modules"
+      ],
+
+      extensions: [".js"],
+
+      // 别名
+      alias: {}
+    },
+
+    devServer: {
+      contentBase: outputPath,
+      compress: true,
+      host: '192.168.1.222',
+      port: 3001,
+      hot: false
     }
+  }
 }
