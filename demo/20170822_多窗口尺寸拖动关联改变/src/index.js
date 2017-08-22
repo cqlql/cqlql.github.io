c223967cae2e9681436b7e322a95983f39fd965e
cql <cql.ql@qq.com>
2017-08-22 23:21:30 +0800

    效果：多窗口尺寸拖动

--- demo/20170822_多窗口尺寸拖动关联改变/src/index.js
+++ demo/20170822_多窗口尺寸拖动关联改变/src/index.js
@@ -3,12 +3,86 @@ import './index.css'
 
 import dragRelateSize from './js/drag-relate-size'
 
-let leftNav = document.querySelector('.type-menu')
+let typeMenu = document.querySelector('.type-menu')
 let menu = document.querySelector('.menu')
 
-dragRelateSize({
-  elem:leftNav,
+// let initWidth = 200
+let minWidth = 10
+let menuWidth = 200
+let maxMenuWidth
+let typeMenuHandle = dragRelateSize({
+  elem:typeMenu,
+  minWidth,
+  rightWidth:menuWidth,
+  onDown(){
+    typeMenuHandle.setRightWidth(menuWidth)
+    typeMenuHandle.setMaxWidth()
+    maxMenuWidth = typeMenuHandle.getMaxWidth()
+  },
   onMove(x){
-    menu.style.width = 200-x+ 'px'
+    menuWidth -= x
+
+    if (menuWidth < minWidth) {
+      menuWidth = minWidth
+    }
+    else if(menuWidth>maxMenuWidth){
+      menuWidth = maxMenuWidth
+    }
+
+    menu.style.width = menuWidth + 'px'
+  },
+  onUp(){
+
+  }
+})
+
+let leftNav = document.querySelector('.left-nav')
+let rightCont = document.querySelector('.right-cont')
+
+let leftNavWidht = menuWidth + typeMenuHandle.getWidth()
+let rightContLeft=leftNavWidht+10
+let minLeftNavWidht
+let maxLeftNavWidth
+let minRightContLeft
+let maxRightContLeft
+let menuHandle = dragRelateSize({
+  elem:menu,
+  minWidth,
+  initWidth:menuWidth,
+  onDown(){
+    let rightContWidth = rightCont.clientWidth
+    minLeftNavWidht = typeMenuHandle.getWidth() + minWidth
+    minRightContLeft=minLeftNavWidht+10
+
+    maxRightContLeft = leftNavWidht + rightContWidth
+    maxLeftNavWidth = maxRightContLeft +10
+
+    menuHandle.setRightWidth(rightContWidth)
+
+    menuHandle.setWidth(menuWidth)
+
+    menuHandle.setMaxWidth()
+  },
+  onMove(x){
+    leftNavWidht+=x
+    rightContLeft+=x
+
+    if(leftNavWidht<minLeftNavWidht){
+      leftNavWidht=minLeftNavWidht
+    } else if(leftNavWidht>maxLeftNavWidth){
+      leftNavWidht = maxLeftNavWidth
+    }
+
+    if(rightContLeft<minRightContLeft){
+      rightContLeft=minRightContLeft
+    } else if(rightContLeft>maxRightContLeft){
+      rightContLeft = maxRightContLeft
+    }
+
+    leftNav.style.width = leftNavWidht + 'px'
+    rightCont.style.marginLeft = rightContLeft + 'px'
+  },
+  onUp(){
+    menuWidth = menuHandle.getWidth()
   }
 })
--- demo/20170822_多窗口尺寸拖动关联改变/src/js/drag-relate-size.js
+++ demo/20170822_多窗口尺寸拖动关联改变/src/js/drag-relate-size.js
@@ -1,16 +1,17 @@
-
 /**
  *
- * 必须确保参数 elem 非 static 定位
+ * 参数 elem 在非 static 定位的情况会自动设置为 relative
+ *
+ * 目前只实现 x 方向
  * */
 
 import {dragPlus} from 'corejs'
 
-export default function dragRelateSize({elem,onMove}) {
+export default function dragRelateSize({elem,onDown, onMove,onUp=()=>{},minWidth=0,rightWidth,initWidth}) {
 
   let bar = document.createElement('div')
 
-  bar.setAttribute('style',`position:absolute;
+  bar.setAttribute('style', `position:absolute;
     right: -5px;
     top: 0;
     bottom: 0;
@@ -18,16 +19,58 @@ export default function dragRelateSize({elem,onMove}) {
 
   elem.appendChild(bar)
 
-  let width = parseFloat(window.getComputedStyle(elem, null).getPropertyValue('width'))
-  let left=0
+  let style = window.getComputedStyle(elem, null)
+  let isStatic = style.getPropertyValue('position') === 'static'
+  initWidth = initWidth===undefined? parseFloat(style.getPropertyValue('width')):initWidth
+
+  if(isStatic){
+    elem.style.position  = 'relative'
+  }
+
+  let width = initWidth
+  let maxWidth = setMaxWidth()
   dragPlus({
-    eDrag:bar,
-    onMove({x}){
-      left+=x
-      elem.style.width = width+left+'px'
-      onMove(left)
-    }
+    eDrag: bar,
+    onDown(e) {
+      onDown()
+
+      if (e.cancelable) e.preventDefault()
+    },
+    onMove({x, event}) {
+
+      width += x
+
+      if (width < minWidth) {
+        width = minWidth
+      } else if(width>maxWidth){
+        width = maxWidth
+      }
+
+      onMove(x)
+
+      elem.style.width = width + 'px'
+      //
+    },
+    onUp
   })
 
-}
+  function setMaxWidth() {
+    return maxWidth = rightWidth + width -minWidth
+  }
 
+  return {
+    setWidth(w){
+      width = w
+    },
+    getWidth(){
+      return width
+    },
+    getMaxWidth(){
+      return maxWidth
+    },
+    setRightWidth(w){
+      rightWidth = w
+    },
+    setMaxWidth
+  }
+}
