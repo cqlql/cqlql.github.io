b50fe6eb5b531c421d664dbfab48f2db09be1e1a
cqlql <cql.ql@qq.com>
2017-08-23 12:12:10 +0800

    新增效果:多窗口尺寸拖动关联改变

--- demo/20170822_多窗口尺寸拖动关联改变/src/index.js
+++ demo/20170822_多窗口尺寸拖动关联改变/src/index.js
@@ -1,88 +1,39 @@
 
 import './index.css'
 
-import dragRelateSize from './js/drag-relate-size'
+// import DragRelateSize from './js/drag-relate-size'
+// import MoveHandle from './js/move-handle'
+import {MoveHandle,DragRelateSize} from 'drag-relate-size'
 
 let typeMenu = document.querySelector('.type-menu')
 let menu = document.querySelector('.menu')
-
-// let initWidth = 200
-let minWidth = 10
-let menuWidth = 200
-let maxMenuWidth
-let typeMenuHandle = dragRelateSize({
-  elem:typeMenu,
-  minWidth,
-  rightWidth:menuWidth,
-  onDown(){
-    typeMenuHandle.setRightWidth(menuWidth)
-    typeMenuHandle.setMaxWidth()
-    maxMenuWidth = typeMenuHandle.getMaxWidth()
-  },
-  onMove(x){
-    menuWidth -= x
-
-    if (menuWidth < minWidth) {
-      menuWidth = minWidth
-    }
-    else if(menuWidth>maxMenuWidth){
-      menuWidth = maxMenuWidth
-    }
-
-    menu.style.width = menuWidth + 'px'
-  },
-  onUp(){
-
-  }
-})
-
 let leftNav = document.querySelector('.left-nav')
 let rightCont = document.querySelector('.right-cont')
 
-let leftNavWidht = menuWidth + typeMenuHandle.getWidth()
-let rightContLeft=leftNavWidht+10
-let minLeftNavWidht
-let maxLeftNavWidth
-let minRightContLeft
-let maxRightContLeft
-let menuHandle = dragRelateSize({
-  elem:menu,
-  minWidth,
-  initWidth:menuWidth,
+let leftNavDrag=new DragRelateSize({
+  elem:leftNav,
   onDown(){
-    let rightContWidth = rightCont.clientWidth
-    minLeftNavWidht = typeMenuHandle.getWidth() + minWidth
-    minRightContLeft=minLeftNavWidht+10
+    leftNavDrag.minWidth = typeMenuDrag.currWidth + 10
+    rightContMove.minWidth = leftNavDrag.minWidth + 10
+    rightContMove.maxWidth = leftNavDrag.maxWidth = leftNavDrag.currWidth + rightCont.clientWidth-10
+    leftNavDrag.maxWidth = rightContMove.maxWidth -10
 
-    maxRightContLeft = leftNavWidht + rightContWidth
-    maxLeftNavWidth = maxRightContLeft +10
-
-    menuHandle.setRightWidth(rightContWidth)
-
-    menuHandle.setWidth(menuWidth)
-
-    menuHandle.setMaxWidth()
+    rightContMove.down()
   },
   onMove(x){
-    leftNavWidht+=x
-    rightContLeft+=x
-
-    if(leftNavWidht<minLeftNavWidht){
-      leftNavWidht=minLeftNavWidht
-    } else if(leftNavWidht>maxLeftNavWidth){
-      leftNavWidht = maxLeftNavWidth
-    }
-
-    if(rightContLeft<minRightContLeft){
-      rightContLeft=minRightContLeft
-    } else if(rightContLeft>maxRightContLeft){
-      rightContLeft = maxRightContLeft
-    }
-
-    leftNav.style.width = leftNavWidht + 'px'
-    rightCont.style.marginLeft = rightContLeft + 'px'
-  },
-  onUp(){
-    menuWidth = menuHandle.getWidth()
+    rightContMove.move(x,function (x) {
+      rightCont.style.marginLeft = x + 'px'
+    })
   }
 })
+
+let rightContMove = new MoveHandle({initWidth:leftNavDrag.currWidth + 10})
+
+let typeMenuDrag=new DragRelateSize({
+  elem:typeMenu,
+  onDown(){
+    typeMenuDrag.maxWidth = leftNavDrag.currWidth - 10
+  }
+})
+
+
