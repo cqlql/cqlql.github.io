2fa389ebbe6a9d97c40252ca2b7b0fee8d0bcb8e
cql <cql.ql@qq.com>
2017-09-16 18:39:03 +0800

    笔记、效果：惯性定位

--- demo/20170104_滑动惯性定位/src/index.js
+++ demo/20170104_滑动惯性定位/src/index.js
@@ -1,16 +1,20 @@
 import './index.css'
 import Swipe from './swipe'
 import drag from './modules/drag'
+// import drag from './modules/drag-mobile'
 import Figure from './modules/figure'
 import Animation from './modules/animation'
 import InertiaAnime from './modules/inertia-anime'
 import generateAnimeData from './modules/generate-anime-data'
+import autoprefix from './modules/autoprefix'
+
+var transform = autoprefix('transform')[1]
 
 let swipeBase = new Swipe();
 let animation = new Animation
 let figure = new Figure
 animation.easing = function (x, t, b, c, d) {
-  return c * ((t = t / d - 1) * t * t + 1) + b;
+  return -c * (t /= d) * (t - 2) + b;
 };
 let eDrag = document.getElementById('app')
 
@@ -34,14 +38,16 @@ inertiaAnime.complete = function () {
 
 // firstIndex=startAnimeData.length-1
   function run() {
-    eDrag.style.top = startAnimeData[i] + 'px'
+    // eDrag.style.top = startAnimeData[i] + 'px'
+    eDrag.style[transform] = 'translate3d(0,'+startAnimeData[i]+'px,0)'
     i++
     if (i > firstIndex) {
       let s = startAnimeData[firstIndex]
       let len = end - s
       animation.start(function (p) {
-        eDrag.style.top = s + len * p + 'px'
-      }, 200)
+        // eDrag.style.top = s + len * p + 'px'
+        eDrag.style[transform] = 'translate3d(0,'+(s + len * p)+'px,0)'
+      }, 400)
 
       return
     }
@@ -80,4 +86,35 @@ drag({
     })
   }
 });
+
+/*
+drag({
+  eDrag,
+  onStart(e) {
+    e.stopPropagation();
+
+    startX = e.touches[0].pageY
+    animeData=[]
+    swipeBase.start(startX)
+
+  },
+  onMove(e) {
+    let moveX = e.touches[0].pageY
+
+    let lenx = moveX - startX
+
+    x = lenx + tpx
+    swipeBase.move(moveX)
+    eDrag.style.top = x + 'px'
+    e.preventDefault();
+  },
+
+  onEnd(e) {
+    swipeBase.end(function (r) {
+      inertiaAnime.start(inertiaAnime.cur + r * 160)
+
+    })
+  }
+});
+*/
 
--- demo/20170104_滑动惯性定位/src/modules/generate-anime-data.js
+++ demo/20170104_滑动惯性定位/src/modules/generate-anime-data.js
@@ -6,13 +6,13 @@
 
 let itemH = 38
 
-/*easeOutCubic*/
+/*easeOutQuad*/
 function ease(x, t, b, c, d) {
-  return c * ((t = t / d - 1) * t * t + 1) + b;
+  return -c * (t /= d) * (t - 2) + b;
 }
 
 let interval = 20 // 帧间隔
-let count = 200 / interval // 总次数
+let t = 400 / interval // 总次数
 
 export default function (animeData) {
 
@@ -24,19 +24,19 @@ export default function (animeData) {
   let ld = animeData[count - 1]
   let lastLen = itemH - ld % itemH
 
-  if(ld-animeData[0]>0){
-    lastLen = itemH - ld % itemH
-  } else{
-    lastLen = itemH + ld % itemH
-  }
+  // if(ld-animeData[0]>0){
+  //   lastLen = itemH - ld % itemH
+  // } else{
+  //   lastLen = itemH + ld % itemH
+  // }
   let end = lastLen + ld
   let firstIndex
   for (let i = count - 1, d, lsp; i--;) {
     d = animeData[i]
     lsp = d - animeData[i - 1]
-    console.log(lsp)
-    if((lsp>0&&ease( null,1,0,ld-d+lastLen,count)<lsp)||ease( null,1,0,ld-d+lastLen,count)>lsp){
-    // if (ease(null, 1, 0, ld - d + lastLen, count) < lsp) {
+
+    // if((lsp>0&&ease( null,1,0,ld-d+lastLen,t)<lsp)||ease( null,1,0,ld-d+lastLen,t)>lsp){
+    if (ease(null, 1, 0, ld - d + lastLen, count) < lsp) {
       firstIndex = i
       // let s = animeData[i+1]
       // s+ld-s
