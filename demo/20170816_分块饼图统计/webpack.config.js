00ec4955af33dd33cba5c58593e9d84e2959c4b2
桥黎 <cql.ql@qq.com>
2017-11-08 18:22:51 +0800

    修改工程，笔记，demo

--- demo/20170816_分块饼图统计/webpack.config.js
+++ demo/20170816_分块饼图统计/webpack.config.js
@@ -1,112 +1,29 @@
+/* dev 用。因为使用的是默认的 devServer */
 let path = require('path');
-let webpack = require('webpack');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
-let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
-// let CleanWebpackPlugin = require('clean-webpack-plugin');
+let getIPAdress = require('../../build/get-ip-adress')
+let baseConf = require('./webpack.base')
+let merge = require('webpack-merge')
 
-module.exports = function (env, options) {
+baseConf.entry.main.push('./src/test-data') // 增加测试数据js
 
-  let params = env ? env.split(',') : []
-  let dev = params[0] !== 'p'
-
-  let outputPath = path.resolve(__dirname, "dist")
-
-  return {
-    entry:{
-      index: ["./src/index.js"]
-    },
-
-    output: {
-      path: outputPath,
-      filename: "js/[name].js"
-    },
+let conf = {
     plugins: [
-      new HtmlWebpackPlugin({
-        filename: './index.html',
-        template: './src/index.html',
-        chunks: ['index'],
-        inlineSource: '.(js|css)$',
-        minify:{
-          removeComments: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true,
-          minifyCSS: true
-          // more options:
-          // https://github.com/kangax/html-minifier#options-quick-reference
-        }
-      }),
-      new ExtractTextPlugin('css/[name].css'),
-      new HtmlWebpackInlineSourcePlugin()
-    ].concat(dev?[]:[
-      new webpack.optimize.UglifyJsPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
-    ]),
-
-    module: {
-      //加载器配置
-      rules: [
-        {
-          test: /\.js$/,
-          // exclude: /node_modules/,
-          include: [
-            path.resolve(__dirname, "src"),
-            'E:\\github\\cqlql.github.io\\modules'
-          ],
-          loader: 'babel-loader',
-          options: {
-            "presets": ["env"],
-            "plugins": ["transform-runtime", "syntax-dynamic-import"],
-          }
-        }, {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            // fallback: 'style-loader',
-            use: [{
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-                sourceMap: true,
-                url: false
-              }
-            }, {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [
-                  require('autoprefixer')({
-                    remove: false
-                  }),
-                ]
-              }
-            }]
-          }),
-        }
-      ]
-    },
-    resolve: {
-
-      // 寻找模块的目录
-      modules: [
-        path.resolve(__dirname, "../../node_modules")
-        // "node_modules"
-      ],
-
-      extensions: [".js"],
-
-      // 别名
-      alias: {}
-    },
-
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './src/index.html',
+            chunks: ['main']
+        }),
+    ],
     devServer: {
-      contentBase: outputPath,
-      compress: true,
-      host: '192.168.1.222',
-      port: 1234,
-      hot: false
-    }
-  }
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        // host: '192.168.1.222',
+        host: getIPAdress(),
+        port: 3002,
+        // openPage: 'http://192.168.1.222',
+        inline: true
+      }
 }
+
+module.exports = merge(baseConf, conf)
