333bdbdc6241742a6f9051fb7bdcf27132eab521
cql <cql.ql@qq.com>
2017-11-08 19:23:07 +0800

    效果

---
+++ demo/20170920_底部弹窗/src/popup-bottom/popup-bottom.js
@@ -0,0 +1,41 @@
+export default class {
+  init () {
+    let html = `
+<div class="popup-buttom">
+  <div class="fgp-bg"></div>
+  <div class="fgp-bd">
+    <div class="fgp-cont"><!--内容--></div>
+  </div>
+</div>`
+    let elm = document.createElement('div')
+    elm.innerHTML = html
+
+    let elRoot = this.elRoot = elm.children[0]
+    let elChild = elRoot.children
+
+    let elBg = this.elBg = elChild[0]
+    this.elCont = elChild[1]
+
+    elBg.addEventListener('click', () => {
+      this.close()
+    })
+
+    document.body.appendChild(elRoot)
+  }
+
+  show () {
+    this.elRoot.classList.add('show')
+  }
+
+  close () {
+    this.elRoot.classList.remove('show')
+  }
+  setCont (body) {
+    // if (typeof body === 'string'){
+    this.elCont.innerHTML = body
+    // } else{
+    //   elCont.innerHTML = ''
+    //   elCont.appendChild(body)
+    // }
+  }
+}
