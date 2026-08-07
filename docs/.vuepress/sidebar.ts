ce4f88b2084bc9469fe2d94d970ac5cd5c9c584c
cql <cql.ql@qq.com>
2026-08-07 18:21:34 +0800

    feat(docs): 支持侧边栏多级菜单可折叠
    递归设置导航中二级和三级目录的折叠属性，提升文档侧边栏的使用体验

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,10 +3,20 @@ import navbarData, { NavDataItem } from "./utils/nav-generate";
 const sidebarData: Record<string, any[]> = {}
 navbarData.forEach((item: NavDataItem) => {
   if (item.prefix) {
-    sidebarData[item.prefix] = [{
-      ...item,
-      collapsible: true,
-    }]
+    // 二级、三级目录设置可折叠
+    if (item.children && item.children.length > 0) {
+      item.children.forEach((child: any) => {
+        if (child.children && child.children.length > 0) {
+          child.collapsible = true
+          child.children.forEach((grandchild: any) => {
+            if (grandchild.children && grandchild.children.length > 0) {
+              grandchild.collapsible = true
+            }
+          })
+        }
+      })
+    }
+    sidebarData[item.prefix] = [item]
   }
 })
 export default sidebarData
